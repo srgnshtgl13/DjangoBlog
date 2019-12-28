530816ce0af0538c8c61a376cdaf352409864411
Sergen Sahutoglu <48659439+srgnshtgl13@users.noreply.github.com>
2019-12-28 19:20:07 +0300

    Update views.py

--- registration/views.py
+++ registration/views.py
@@ -5,6 +5,8 @@ from django.contrib.auth.models import User
 from django.views import View
 from registration.forms import UserCreateForm
 from django.contrib.auth import authenticate, login, logout
+from userprofile.models import Profile
+from django.utils import timezone
 
 
 class MySignUpView(View):
@@ -27,20 +29,31 @@ class MySignUpView(View):
                 is_active=True
             )"""
             user = form.save(commit=False)
-            user.username=form.cleaned_data.get('username')
+            user.username = form.cleaned_data.get('username')
             user.email = request.POST['email']
             user.set_password(form.cleaned_data.get('password1'))
             user.is_active = True
             user.first_name = request.POST['first_name']
             user.last_name = request.POST['last_name']
             user.save()
-            new_user = authenticate(username=form.cleaned_data.get('username'), password=form.cleaned_data.get('password1'))
+            new_user = authenticate(username=form.cleaned_data.get(
+                'username'), password=form.cleaned_data.get('password1'))
             login(request, new_user)
+            
+            # Fix Visiting UserProfile Page Error: if we don't create a profile object and save the field as null 
+            # we will get an error when we visit the user profile file
+            profile = Profile()
+            profile.user = new_user
+            profile.department = ""
+            profile.bio = ""
+            profile.location = ""
+            profile.save()
             # TODO Display message and redirect to login
-            #return HttpResponseRedirect('/accounts/login/?next=/')
+            # return HttpResponseRedirect('/accounts/login/?next=/')
             # TODO Display message and redirect to home page
             return redirect('home')
         return render(request, self.template_name, {'form': form})
+
 
 class MyLogoutView(View):
     def get(self, request, *args, **kwargs):
